6e9b45e5b4cabc447afddc9d34e49971c0fc8911
dyamijr <dyamijr@yahoo.com>
2024-12-06 11:15:38 -0500

    updated add

--- src/database.cpp
+++ src/database.cpp
@@ -15,78 +15,76 @@ auto findIndexAndDoc(auto indexTable, string index, string docId){
 
     // Find the document
     auto result = indexTable.find_one(query.view());
-
+    
     return result; 
 }
 
-void addIndex(auto dbClient, string index, string docId, int frequency, vector<int> positions){
+bool addIndex(auto dbClient, string index, string docId, int frequency, vector<int> positions){
     // Step 1: Query to find the document with the given index and DocId
     auto db = dbClient["IndexingDB"];
     auto indexTable = db["indextable"];
-    auto query = findIndexAndDoc(indexTable, index, docId);
+    //auto query = findIndexAndDoc(indexTable, index, docId);
     
-    // Step 2: Define the update operation for updating the frequency and positions
-    auto update = bsoncxx::builder::stream::document{}
-                  << "$set" << bsoncxx::builder::stream::open_document
-                  << "Documents.$.frequency" << frequency
-                  << bsoncxx::builder::stream::close_document;
-
-    update << "$push" << bsoncxx::builder::stream::open_document
-           << "Documents.$.positions" << bsoncxx::builder::stream::open_array;
-    for (int pos : positions) {
-        update << pos;
-    }
-    update << bsoncxx::builder::stream::close_array
-           << bsoncxx::builder::stream::close_document
-           << bsoncxx::builder::stream::finalize;
-
-    // Step 3: If no matching document found for update, add a new document
-    auto addNewDoc = bsoncxx::builder::stream::document{}
-                       << "$push" << bsoncxx::builder::stream::open_document
-                       << "Documents" << bsoncxx::builder::stream::open_document
-                       << "DocId" << docId
-                       << "frequency" << frequency
-                       << "positions" << bsoncxx::builder::stream::open_array;
-    for (int pos : positions) {
-        addNewDoc << pos;
-    }
-    addNewDoc << bsoncxx::builder::stream::close_array
-              << bsoncxx::builder::stream::close_document
-              << bsoncxx::builder::stream::close_document
-              << bsoncxx::builder::stream::finalize;
-
-    // Step 4: Check if the document with index and DocId exists, and update or insert
-    auto result = indexTable.update_one(query.view(), update.view());
-
-    if ( !(result && result->modified_count() > 0)) {
-        // If no matching document was updated, check if the index exists
-        auto indexQuery = bsoncxx::builder::stream::document{}
-                           << "index" << index
-                           << bsoncxx::builder::stream::finalize;
-
-        // Try inserting the new document if the index exists but DocId does not
-        result = indexTable.update_one(indexQuery.view(), addNewDoc.view());
-        if ( !(result && result->modified_count() > 0) ) {
-            // If index does not exist, insert the full document
-            auto fullInsert = bsoncxx::builder::stream::document{}
-                               << "index" << index
-                               << "Documents" << bsoncxx::builder::stream::open_array
-                               << bsoncxx::builder::stream::open_document
-                               << "DocId" << docId
-                               << "frequency" << frequency
-                               << "positions" << bsoncxx::builder::stream::open_array;
-            for (int pos : positions) {
-                fullInsert << pos;
-            }
-            fullInsert << bsoncxx::builder::stream::close_array
-                       << bsoncxx::builder::stream::close_document
-                       << bsoncxx::builder::stream::close_array
-                       << bsoncxx::builder::stream::finalize;
-
-            // Insert the new document
-            indexTable.insert_one(fullInsert.view());
+    //New logic
+    auto query = bsoncxx::builder::stream::document{}
+                 << "index" << index
+                 << "Documents.DocId" << docId
+                 << bsoncxx::builder::stream::finalize;
+    
+    auto result = indexTable.find_one(query.view());
+    int pos = 0;
+    //CHECK IF DOC AND INDEX EXIST
+    if (result){
+        bsoncxx::builder::stream::document update_builder;
+        update_builder << "$push" << bsoncxx::builder::stream::open_document
+                        << "Documents.$.positions" << to_string(pos)  // Append new position
+                        << bsoncxx::builder::stream::close_document;
+        auto newResult = indexTable.update_one(query.view(), update_builder.view());
+        if(newResult && newResult->matched_count() > 0){
+            return true;
+        } else {
+            return false;
         }
     }
+    //CHECK IF INDEX EXIST
+    query = bsoncxx::builder::stream::document{}
+                 << "index" << index
+                 << bsoncxx::builder::stream::finalize;
+    result = indexTable.find_one(query.view());
+    if (result){
+        bsoncxx::builder::stream::document new_doc_builder;
+        new_doc_builder << "DocId" << docId  // New document ID
+                        << "frequency" << frequency
+                        << "positions" << bsoncxx::builder::stream::open_array
+                        << to_string(pos) // New positions
+                        << bsoncxx::builder::stream::close_array;
+        bsoncxx::builder::stream::document update_builder;
+        update_builder << "$push" << bsoncxx::builder::stream::open_document
+                    << "Documents" << new_doc_builder.view()  // Append new document
+                    << bsoncxx::builder::stream::close_document;
+        auto newResult = indexTable.update_one(query.view(), update_builder.view());
+        if(newResult && newResult->matched_count() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    bsoncxx::builder::stream::document new_entry_builder;
+    new_entry_builder << "index" << index  // New 'index' value
+                      << "Documents" << bsoncxx::builder::stream::open_array
+                      << bsoncxx::builder::stream::open_document
+                      << "DocId" << docId
+                      << "frequency" << frequency
+                      << "positions" << bsoncxx::builder::stream::open_array
+                      << to_string(pos)
+                      << bsoncxx::builder::stream::close_array
+                      << bsoncxx::builder::stream::close_document
+                      << bsoncxx::builder::stream::close_array;
+    auto newResult = indexTable.insert_one(new_entry_builder.view());
+    if (newResult) {
+        return true;
+    } 
+    return false;
 }
 
 void removeDoc(auto dbClient, string docId){
